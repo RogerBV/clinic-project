1b08aff527769d55cd2864e5f1a43654d9a9f407
roger.barriga <roger.barriga@scribsoft.com>
2024-09-13 20:29:36 -0500

    Adding print methods

--- backend/app/tests/test_specialization.py
+++ backend/app/tests/test_specialization.py
@@ -7,14 +7,22 @@ from businessRules import SpecializationBR
 
 
 def test_getSpecializations():
+    print("test_getSpecializations 1")
     specializationBR = SpecializationBR()
+    print("test_getSpecializations 2")
     list = specializationBR.getSpecializations()
+    print("test_getSpecializations 3")
     assert len(list) == 0
     
 def test_insertSpecialzation():
+    print("test_insertSpecialzation 1")
     specializationNameTest = "Test 1"
+    print("test_insertSpecialzation 2")
     specializationBR = SpecializationBR()
+    print("test_insertSpecialzation 3")
     objSpecialization = specializationBR.insertSpecialization(specializationNameTest)
+    print("test_insertSpecialzation 4")
     objSpecializationTest = specializationBR.getSpecializationById(objSpecialization.id)
-    assert objSpecializationTest.specializationName == specializationNameTest
+    print("test_insertSpecialzation 5")
+    assert objSpecializationTest.specializationName.__eq__(specializationNameTest)
         
